54ae7c4e3ee518cc88090180e2cc39bb3597ba40
Urte Vycinaite <urte.vycinaite@gmail.com>
2023-10-11 19:32:20 +0300

    add new exercise

---
+++ main.py
@@ -0,0 +1,6 @@
+# Create a program witch takes 5 different (minmum)  variables containing strings for username and password.
+# The string should look like : "username='', password=''; username='',password='';....etc"
+# Start Endless loop allowing to enter username and password and 
+# # check ff credentials are correct stop endless loop and print greeting message, otherwise print 'wrong credentials' and ask to enter password and username again. The program should show numbers of times you tried to enter both credentials.
+# For storing account information use dict , all data should be lowercase. and extra symbols (@, %, $ etc and numbers). If in password numbers or symbols are not provided, the program should add additional value to dictionary as a value, names 'improved password'
+
